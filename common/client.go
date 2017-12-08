9c4a08d93cc3b353ba8560099822a1c870c27ac2
Esteban G <esteban.ginez@oracle.com>
2017-12-07 16:23:26 -0800

    #4 adding extra env lookup to cover windows os

--- common/client.go
+++ common/client.go
@@ -119,8 +119,12 @@ func NewClientWithConfig(configProvider ConfigurationProvider) (client BaseClien
 func getHomeFolder() string {
 	current, e := user.Current()
 	if e != nil {
-		//Give up and return something sensible
-		return os.Getenv("HOME")
+		//Give up and try to return something sensible
+		home :=  os.Getenv("HOME")
+		if home == "" {
+			home = os.Getenv("USERPROFILE")
+		}
+		return home
 	}
 	return current.HomeDir
 }
