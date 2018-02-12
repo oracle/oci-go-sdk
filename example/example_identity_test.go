914a42440573c1d6e1b5b121f654f5d70f6dd949
Jason Yin <jason.yin@oracle.com>
2018-02-11 22:04:08 -0800

    address code review comments

--- example/example_identity_test.go
+++ example/example_identity_test.go
@@ -6,35 +6,33 @@
 // The API for the Database Service.
 //
 
-package identitySamples
+package example
 
 import (
 	"context"
 	"fmt"
 
-	"github.com/oracle/oci-go-sdk/example"
-
 	"github.com/oracle/oci-go-sdk/common"
 	"github.com/oracle/oci-go-sdk/identity"
 )
 
-// ListAvailabilityDomains Lists the Availability Domains in your tenancy.
+// ExampleListAvailabilityDomains Lists the Availability Domains in your tenancy.
 // Specify the OCID of either the tenancy or another of your compartments as
 // the value for the compartment ID (remember that the tenancy is simply the root compartment).
-func ListAvailabilityDomains() {
+func ExampleListAvailabilityDomains() {
 	c, err := identity.NewIdentityClientWithConfigurationProvider(common.DefaultConfigProvider())
-	example.LogIfError(err)
+	LogIfError(err)
 
 	// The OCID of the tenancy containing the compartment.
 	tenancyID, err := common.DefaultConfigProvider().TenancyOCID()
-	example.LogIfError(err)
+	LogIfError(err)
 
 	request := identity.ListAvailabilityDomainsRequest{
 		CompartmentId: &tenancyID,
 	}
 
 	r, err := c.ListAvailabilityDomains(context.Background(), request)
-	example.LogIfError(err)
+	LogIfError(err)
 
 	fmt.Printf("List of available domains: %v", r.Items)
 	return
