18eb1c0a6347674a4bba8799f06b1200b65ed7db
Jason Yin <jason.yin@oracle.com>
2018-04-04 04:14:05 +0000

    Merge pull request #24 in SDK/oci-go-sdk from jayin/serviceupdate to preview
    SCOPE: PREVIEW
    
    This is service update with retry.
    
    Squashed commit of the following:
    
    commit d0af1859de7821465d87be6cbd3212c1b856c01d
    Merge: 7693101 4ed8c7a
    Author: Jason Yin <jason.yin@oracle.com>
    Date:   Tue Apr 3 20:53:29 2018 -0700
    
        Merge remote-tracking branch 'origin/preview' into jayin/serviceupdate
    
    commit 769310104a74fb6dc99efc3c16e40d72e9a1319e
    Merge: 1e912d7 bf568c7
    Author: Jason Yin <jason.yin@oracle.com>
    Date:   Tue Apr 3 20:42:50 2018 -0700
    
        Merge remote-tracking branch 'origin/preview' into jayin/serviceupdate
    
    commit 1e912d772cf68f2740bd38d987ecbc0d9656801d
    Author: Jason Yin <jason.yin@oracle.com>
    Date:   Mon Apr 2 16:27:04 2018 -0700
    
        regenerate
    
    commit 18fafb1fd70f2081ae6890bda7f94c172e63b65e
    Author: Jason Yin <jason.yin@oracle.com>
    Date:   Thu Mar 29 11:36:10 2018 -0700
    
        add test case for list buckets which taks Fields parameter
    
    commit bea4632c11c43db6fd743482c9116a98ad80787c
    Author: Jason Yin <jason.yin@oracle.com>
    Date:   Wed Mar 28 22:58:48 2018 -0700
    
        add sample and test for email services
    
    commit b1fea461bea8fa778419464b5d93cd827bb338b4
    Author: Jason Yin <jason.yin@oracle.com>
    Date:   Wed Mar 28 21:53:54 2018 -0700
    
        service updates and email service

---
+++ integtest/email_client_integ_test.go
@@ -0,0 +1,68 @@
+// Copyright (c) 2016, 2018, Oracle and/or its affiliates. All rights reserved.
+//
+// Email Delivery Service Tests
+//
+// API spec for managing OCI Email Delivery services.
+//
+
+package integtest
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/oracle/oci-go-sdk/common"
+	"github.com/oracle/oci-go-sdk/email"
+)
+
+// sanity test for email service
+func TestEmailClient_EmailSender(t *testing.T) {
+	client, err := email.NewEmailClientWithConfigurationProvider(common.DefaultConfigProvider())
+	failIfError(t, err)
+
+	testEmailAddress := "gosdkintegtest@test.com"
+
+	ctx := context.Background()
+
+	createReq := email.CreateSenderRequest{
+		CreateSenderDetails: email.CreateSenderDetails{
+			CompartmentId: common.String(getCompartmentID()),
+			EmailAddress:  common.String(testEmailAddress),
+		},
+	}
+
+	createResp, err := client.CreateSender(ctx, createReq)
+	failIfError(t, err)
+	assert.Equal(t, testEmailAddress, *createResp.EmailAddress)
+
+	getReq := email.GetSenderRequest{
+		SenderId: createResp.Id,
+	}
+
+	getResp, err := client.GetSender(ctx, getReq)
+	failIfError(t, err)
+	assert.Equal(t, testEmailAddress, *getResp.EmailAddress)
+
+	// you can provide additional filters and sorts, here lists all senders
+	// sorted by email address and filter by email address
+	listReq := email.ListSendersRequest{
+		CompartmentId: common.String(getCompartmentID()),
+		SortBy:        email.ListSendersSortByEmailaddress,
+		SortOrder:     email.ListSendersSortOrderAsc,
+	}
+
+	listResp, err := client.ListSenders(ctx, listReq)
+	failIfError(t, err)
+	assert.Equal(t, 1, len(listResp.Items))
+
+	defer func() {
+		deleteReq := email.DeleteSenderRequest{
+			SenderId: getReq.SenderId,
+		}
+
+		_, err = client.DeleteSender(ctx, deleteReq)
+		failIfError(t, err)
+	}()
+}
