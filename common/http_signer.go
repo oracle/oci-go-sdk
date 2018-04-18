40573fb78157b4764efa1e7d7379817aee772fa2
Owain Lewis <owain.lewis@oracle.com>
2018-04-18 10:39:20 +0100

    Implement support for signing patch requests

--- common/http_signer.go
+++ common/http_signer.go
@@ -46,7 +46,7 @@ var (
 	defaultGenericHeaders    = []string{"date", "(request-target)", "host"}
 	defaultBodyHeaders       = []string{"content-length", "content-type", "x-content-sha256"}
 	defaultBodyHashPredicate = func(r *http.Request) bool {
-		return r.Method == http.MethodPost || r.Method == http.MethodPut
+		return r.Method == http.MethodPost || r.Method == http.MethodPut || r.Method == http.MethodPatch
 	}
 )
 
