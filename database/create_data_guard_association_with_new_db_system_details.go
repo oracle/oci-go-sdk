1f41ed06201651d95f856aa93a823f099b684691
Esteban G <esteban.ginez@oracle.com>
2018-12-12 12:38:21 -0800

    Fixing versions of pom files for services due to an incomplete merge

--- database/create_data_guard_association_with_new_db_system_details.go
+++ database/create_data_guard_association_with_new_db_system_details.go
@@ -25,15 +25,15 @@ type CreateDataGuardAssociationWithNewDbSystemDetails struct {
 	// **The password MUST be the same as the primary admin password.**
 	DatabaseAdminPassword *string `mandatory:"true" json:"databaseAdminPassword"`
 
-	// The user-friendly name for the DB System to create the standby database on. It does not have to be unique.
+	// The user-friendly name of the DB system that will contain the the standby database. The display name does not have to be unique.
 	DisplayName *string `mandatory:"false" json:"displayName"`
 
-	// The name of the Availability Domain that the standby database DB System will be located in.
+	// The name of the availability domain that the standby database DB system will be located in. For example- "Uocm:PHX-AD-1".
 	AvailabilityDomain *string `mandatory:"false" json:"availabilityDomain"`
 
-	// The OCID of the subnet the DB System is associated with.
+	// The OCID of the subnet the DB system is associated with.
 	// **Subnet Restrictions:**
-	// - For 1- and 2-node RAC DB Systems, do not use a subnet that overlaps with 192.168.16.16/28
+	// - For 1- and 2-node RAC DB systems, do not use a subnet that overlaps with 192.168.16.16/28
 	// These subnets are used by the Oracle Clusterware private interconnect on the database instance.
 	// Specifying an overlapping subnet will cause the private interconnect to malfunction.
 	// This restriction applies to both the client subnet and backup subnet.
@@ -45,7 +45,7 @@ type CreateDataGuardAssociationWithNewDbSystemDetails struct {
 	// The protection mode to set up between the primary and standby databases. For more information, see
 	// Oracle Data Guard Protection Modes (http://docs.oracle.com/database/122/SBYDB/oracle-data-guard-protection-modes.htm#SBYDB02000)
 	// in the Oracle Data Guard documentation.
-	// **IMPORTANT** - The only protection mode currently supported by the Database Service is MAXIMUM_PERFORMANCE.
+	// **IMPORTANT** - The only protection mode currently supported by the Database service is MAXIMUM_PERFORMANCE.
 	ProtectionMode CreateDataGuardAssociationDetailsProtectionModeEnum `mandatory:"true" json:"protectionMode"`
 
 	// The redo transport type to use for this Data Guard association.  Valid values depend on the specified `protectionMode`:
@@ -55,7 +55,7 @@ type CreateDataGuardAssociationWithNewDbSystemDetails struct {
 	// For more information, see
 	// Redo Transport Services (http://docs.oracle.com/database/122/SBYDB/oracle-data-guard-redo-transport-services.htm#SBYDB00400)
 	// in the Oracle Data Guard documentation.
-	// **IMPORTANT** - The only transport type currently supported by the Database Service is ASYNC.
+	// **IMPORTANT** - The only transport type currently supported by the Database service is ASYNC.
 	TransportType CreateDataGuardAssociationDetailsTransportTypeEnum `mandatory:"true" json:"transportType"`
 }
 
