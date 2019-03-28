48a5eb0a2e5f3dbdd4990dfe2d5ae08053fef0ca
Esteban Ginez <esteban.ginez@oracle.com>
2019-03-28 20:19:44 +0000

    Auto Generated Public Release for oci-go-sdk DEXREQ-357
    Squashed commit of the following:
    
    commit 2eeaacfbbab9075476313a24c1c548765b541d79
    Author: DEXREQ Automation <noreply+dexreq@oracle.com>
    Date:   Wed Mar 6 17:27:43 2019 +0000
    
        Author: DEXREQ Automation <noreply+dexreq@oracle.com>
        Date:  Wed, 6 Mar 2019 17:27:43 +0000
        Running generation for: [[DEXREQ-357]]
    
        Author: DEXREQ Automation <noreply+dexreq@oracle.com>
        Date:  Wed, 6 Mar 2019 17:08:53 +0000
        Updating pom.xml for [[DEXREQ-357]]: Public SDK for UDX-1142
    
        (automatically combined)
    
        Regened manually again by eginez to catchup newest changes

--- autotest/core_virtualnetwork_client_auto_test.go
+++ autotest/core_virtualnetwork_client_auto_test.go
@@ -2302,6 +2302,49 @@ func TestVirtualNetworkClientGetFastConnectProviderService(t *testing.T) {
 }
 
 // IssueRoutingInfo tag="default" email="sic_block_storage_us_grp@oracle.com" jiraProject="BLOCK" opsJiraProject="BS"
+func TestVirtualNetworkClientGetFastConnectProviderServiceKey(t *testing.T) {
+	defer failTestOnPanic(t)
+
+	enabled, err := testClient.isApiEnabled("core", "GetFastConnectProviderServiceKey")
+	assert.NoError(t, err)
+	if !enabled {
+		t.Skip("GetFastConnectProviderServiceKey is not enabled by the testing service")
+	}
+
+	cc, err := testClient.createClientForOperation("core", "VirtualNetwork", "GetFastConnectProviderServiceKey", createVirtualNetworkClientWithProvider)
+	assert.NoError(t, err)
+	c := cc.(core.VirtualNetworkClient)
+
+	body, err := testClient.getRequests("core", "GetFastConnectProviderServiceKey")
+	assert.NoError(t, err)
+
+	type GetFastConnectProviderServiceKeyRequestInfo struct {
+		ContainerId string
+		Request     core.GetFastConnectProviderServiceKeyRequest
+	}
+
+	var requests []GetFastConnectProviderServiceKeyRequestInfo
+	var dataHolder []map[string]interface{}
+	err = json.Unmarshal([]byte(body), &dataHolder)
+	assert.NoError(t, err)
+	err = unmarshalRequestInfo(dataHolder, &requests, testClient.Log)
+	assert.NoError(t, err)
+
+	var retryPolicy *common.RetryPolicy
+	for i, req := range requests {
+		t.Run(fmt.Sprintf("request:%v", i), func(t *testing.T) {
+			retryPolicy = retryPolicyForTests()
+			req.Request.RequestMetadata.RetryPolicy = retryPolicy
+
+			response, err := c.GetFastConnectProviderServiceKey(context.Background(), req.Request)
+			message, err := testClient.validateResult(req.ContainerId, req.Request, response, err)
+			assert.NoError(t, err)
+			assert.Empty(t, message, message)
+		})
+	}
+}
+
+// IssueRoutingInfo tag="default" email="sic_block_storage_us_grp@oracle.com" jiraProject="BLOCK" opsJiraProject="BS"
 func TestVirtualNetworkClientGetIPSecConnection(t *testing.T) {
 	defer failTestOnPanic(t)
 
