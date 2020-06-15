6b5b087d8034d2216d966a1c6e21de440acc4e86
Peng Liu <peng.p.liu@oracle.com>
2020-06-15 18:28:44 +0000

    Releasing Version 20.1.0
    Squashed commit of the following:
    
    commit 81cae1d57c5c46288c62e88e5bdc625b88fcc07d
    Author: pelliu <peng.p.liu@oracle.com>
    Date:   Mon Jun 15 17:46:27 2020 +0000
    
        Releasing version 20 1 0

--- dts/dts_transferappliance_client.go
+++ dts/dts_transferappliance_client.go
@@ -54,7 +54,7 @@ func newTransferApplianceClientFromBaseClient(baseClient common.BaseClient, conf
 
 // SetRegion overrides the region of this client.
 func (client *TransferApplianceClient) SetRegion(region string) {
-	client.Host = common.StringToRegion(region).EndpointForTemplate("dts", "https://datatransfer.{region}.{secondLevelDomain}")
+	client.Host = common.StringToRegion(region).EndpointForTemplate("dts", "https://datatransfer.{region}.oci.{secondLevelDomain}")
 }
 
 // SetConfigurationProvider sets the configuration provider including the region, returns an error if is not valid
