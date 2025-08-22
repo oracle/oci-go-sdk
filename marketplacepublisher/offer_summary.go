7a4722e0a5330ba5be1a594e7a3b41ac8ccc92c1
jesse.millan <jesse.millan@oracle.com>
2025-08-22 15:56:23 -0700

    Update Errorf calls to be compliant with Go 1.24's vet checks.

--- marketplacepublisher/offer_summary.go
+++ marketplacepublisher/offer_summary.go
@@ -89,7 +89,7 @@ func (m OfferSummary) ValidateEnumValue() (bool, error) {
 		errMessage = append(errMessage, fmt.Sprintf("unsupported enum value for OfferStatus: %s. Supported values are: %s.", m.OfferStatus, strings.Join(GetOfferOfferStatusEnumStringValues(), ",")))
 	}
 	if len(errMessage) > 0 {
-		return true, fmt.Errorf(strings.Join(errMessage, "\n"))
+		return true, fmt.Errorf("%s", strings.Join(errMessage, "\n"))
 	}
 	return false, nil
 }
